cfe83ffb3c16a39b9552b9dc2cc9f8559e012516
Ebot Fabien <ebotfabien@Ebots-MacBook-Pro.local>
2024-12-09 05:54:14 +0100

    first commit

---
+++ functions.py
@@ -0,0 +1,59 @@
+import requests
+
+from openai import OpenAI
+from langchain.text_splitter import CharacterTextSplitter,TokenTextSplitter,RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+
+from langchain.schema import Document
+from langchain_openai import OpenAIEmbeddings
+
+
+
+client = OpenAI()
+
+
+
+def get_conversation_chain(documents, query):
+    embeddings = OpenAIEmbeddings()
+    # Prepare documents for FAISS
+    docs = [Document(page_content=doc["text"], embedding=doc["embedding"]) for doc in documents]
+
+    # Create a vector store from the documents
+    vector_store = FAISS.from_documents(docs,embedding=embeddings)
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector_store.as_retriever(),
+        memory=memory
+    )
+    conversation_chain.run(query)
+    return conversation_chain
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        model_name="gpt-4",
+        chunk_size=500,
+        chunk_overlap=0,
+    )
+
+    texts = text_splitter.split_text(text)
+
+    return texts
+
+def embedder(input_text):
+    client = OpenAI()
+    embeddings=client.embeddings.create(
+    model="text-embedding-ada-002",
+    input=input_text,
+    encoding_format="float"
+    )
+    return embeddings.data[0].embedding
---
+++ run.py
@@ -0,0 +1,96 @@
+from flask import Flask, request, jsonify
+from functions import embedder, get_conversation_chain, get_text_chunks
+
+app = Flask(__name__)
+
+# Dummy POST route 1: Add User
+@app.route('/ai/embedding', methods=['POST'])
+def embedding():
+    """
+    Expected JSON for this route:
+    {
+        "text": "my name is john",
+       
+    }
+    """
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    # Extract data
+    text = data.get('text')
+   
+
+    if not text :
+        return jsonify({"error": "Missing required fields:'text'"}), 400
+
+    # Process data
+    vector_data=embedder(text)
+    
+    return jsonify({
+        "message": "Order submitted successfully",
+        "data": {
+            "vector": vector_data,
+        }
+    }), 201
+
+# Dummy POST route 2: Submit Order
+@app.route('/ai/chunks', methods=['POST'])
+def chunks():
+    """
+    This route provides chunks and embedding
+    Expected JSON for this route:
+    {
+        "content": 'This is the content to be passed for chunks'
+    }
+    """
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    # Extract data
+    content = data.get('content')
+   
+
+    if not content :
+        return jsonify({"error": "Missing required fields"}), 400
+
+    # Process data
+
+    segments=get_text_chunks(content)
+    vector_data=embedder(segments)
+    
+    return jsonify({
+        "message": "Order submitted successfully",
+        "data": {
+            "vector": vector_data,
+        }
+    }), 201
+
+# Dummy GET route: Get Items
+@app.route('/ai/response', methods=['POST'])
+def get_response():
+    """
+    Returns an AI response.
+    Expected JSON for this route:
+    {
+        "document": 'This is the content to be passed for chunks',
+        'text':'The searchable text'
+    }
+    """
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+    
+    # Extract data
+    documents = data.get('document')
+    text= data.get('text')
+   
+    response=get_conversation_chain(documents,text)
+
+    return jsonify({
+        "answer": str(response.memory.chat_memory)
+    }), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
